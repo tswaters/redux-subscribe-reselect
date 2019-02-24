8d4527df6a342d8fdbcc890f101c6192cb2a848f
Tyler Waters <tyler.waters@gmail.com>
2019-02-23 22:40:15 -0800

    Fix bad typescrip typings
    Fixes #4

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 import { OutputSelector } from 'reselect';
 import { Unsubscribe, Store } from 'redux';
 
-declare const defaultExport: <S, R, C>(store: Store<S>, selector: OutputSelector<S, R, C>, cb: C) => Unsubscribe;
+declare const defaultExport: <S, R, C>(store: Store<S>, selector: OutputSelector<S, R, C>, cb: (res: R) => void) => Unsubscribe;
 
 export default defaultExport;
